765a545bb887eb3f5440a4ac815ff103b7655087
prodocik <prodocik@users.noreply.github.com>
2017-08-10 14:28:42 -0400

    new pages
    terms and conditions
    pressrelease

---
+++ pressrelease.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="eng">
+
+<head>
+    <meta name="description" content="Location of Georgia Motorcoach. Contact us" />
+    <meta name="keywords" content="Georgia motorcoach contacts, georgia motorcoach office, georgiamotorcoach, RV rentals company, Rent RV in Atlanta" />
+    <title>Georgia Motorcoach - Press releases</title>
+    <?php include ("variables.php");?>
+    <?php include ("assets.php");?>
+</head>
+
+<body class="contacts">
+    <?php include ("menu.php");?>
+    <?php include ("mainMenu.php");?>
+    <div class="container-fluid">
+        <div class="promotext col-lg-12 col-md-12 col-xs-12">
+            <h1>PRESS RELEASES</h1>
+        </div>
+    </div>
+    </header>
+    <div class="content">
+        <div class="about-box">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1>McDonald's Announces Growth Plan for China Following Completion of Strategic Partnership with CITIC and Carlyle</h1>
+                        <p>21.12.2017</p>
+                        <div class="desc">
+                            <p>OAK BROOK, IL and SHANGHAI, CHINA and HONG KONG, CHINA--(Marketwired - Aug 8, 2017) - McDonald's Corporation (NYSE: MCD) ("McDonald's") today announced the successful completion of a strategic partnership with CITIC Limited (SEHK: 00267) ("CITIC"), CITIC Capital Partners ("CITIC Capital"), and The Carlyle Group (NASDAQ: CG) ("Carlyle"). Ramping up a new era of growth and innovation, the partnership will operate and manage McDonald's businesses in mainland China and Hong Kong, leveraging combined expertise and strength to drive an expansion strategy.</p>
+                            <p>The transaction has obtained China's regulatory approval and was completed on July 31, 2017, creating the largest McDonald's franchisee outside of the United States. The sale to the new McDonald's China franchisee includes McDonald's existing businesses in Mainland China (approximately 2,500 restaurants) and Hong Kong (approximately 240 restaurants).</p>
+                            <p>The new partnership today announced a series of development initiatives for mainland China. Termed "Vision 2022," this strategy aims to drive double-digit sales growth in each of the next five years by increasing the number of restaurants from 2,500 to 4,500, including delivery hub coverage of over 75% of restaurants, by the end of 2022, bringing unparalleled convenience to Chinese customers. Opening pace of new McDonald's restaurants in mainland China is expected to progressively ramp up from approximately 250 per year in 2017 to 500 per year in 2022 under the new partnership. In addition, Vision 2022 includes plans to increase significantly McDonald's restaurant portfolio mix in tier 3-4 cities to approximately 45% of all McDonald's restaurants in China. Vision 2022 also includes an increase of "Experience of the Future" restaurants to over 90%, which will enable the brand to offer digitalized and personalized dining experience to more customers.</p>
+                            <p>With innovation hubs located in Hong Kong and Shanghai, McDonald's will strengthen its brand leadership by enhancing the customer experience using menu innovation and advanced digital retail experience.</p>
+                            <p>"China will soon become our largest market outside of the United States. We are excited to join forces with CITIC and Carlyle for better localized decision-making to meet changing customer demands in this dynamic market," said Steve Easterbrook, McDonald's President and CEO. "Mainland China and Hong Kong are leading the global system in capturing new consumer trends such as delivery and digitalization and its driving strong performance and growth momentum. I have great confidence in our new partnership to unlock the full growth potential of China. McDonald's Corporation will continue to play an active part in the China growth journey through our remaining interest and participation on the China Board."</p>
+                            <p>Zhang Yichen, Chairman of the Board of Directors for the new McDonald's China, commented: "The partnership will strengthen McDonald's China's entrepreneurial spirit, driven by ownership at the local level. It will also help us ensure first-class customer service and food safety while accelerating our growth in mainland China and Hong Kong. We believe this is a winning formula that fuses McDonald's global quality standards and branding with CITIC and Carlyle's extensive resources and market expertise in real estate, finance, supply chains, consumer & retail, and technology."</p>
+                            <p> <strong>About McDonald's</strong> 
+                            McDonald's is the world's leading global foodservice retailer with over 37,000 locations in over 100 countries. Approximately 90% of McDonald's restaurants worldwide are owned and operated by independent local business men and women. </p>
+
+                      </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+</body>
+<?php include ("footer.php");?>
+
+</html>
---
+++ termsandcond.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="eng">
+
+<head>
+    <meta name="description" content="Location of Georgia Motorcoach. Contact us" />
+    <meta name="keywords" content="Georgia motorcoach contacts, georgia motorcoach office, georgiamotorcoach, RV rentals company, Rent RV in Atlanta" />
+    <title>Georgia Motorcoach - terms and conditions</title>
+    <?php include ("variables.php");?>
+    <?php include ("assets.php");?>
+</head>
+
+<body class="contacts">
+    <?php include ("menu.php");?>
+    <?php include ("mainMenu.php");?>
+    <div class="container-fluid">
+        <div class="promotext col-lg-12 col-md-12 col-xs-12">
+            <h1>TERMS AND CONDITIONS</h1>
+        </div>
+    </div>
+    </header>
+    <div class="content">
+        <div class="about-box">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1>Terms and conditions</h1>
+                        <p>Last updated: August 10, 2017</p>
+                        <div class="desc">
+                            <h4>Terms and Conditions ("Terms")</h4>
+                            <p>Please read these Terms and Conditions ("Terms", "Terms and Conditions") carefully before using the gegeorgiamotorcoach.com website (the "Service") operated by georgiamotorcoach.com ("us", "we", or "our").</p>
+                            <p>Your access to and use of the Service is conditioned on your acceptance of and compliance with these Terms. These Terms apply to all visitors, users and others who access or use the Service.</p>
+                            <p>By accessing or using the Service you agree to be bound by these Terms. If you disagree with any part of the terms then you may not access the Service. This Terms & Conditions agreement is licensed by TermsFeed to georgiamotorcoach.com.</p>
+                            <h4>Links To Other Web Sites</h4>
+                            <p>Our Service may contain links to third-party web sites or services that are not owned or controlled by georgiamotorcoach.com.</p>
+                            <p>georgiamotorcoach.com has no control over, and assumes no responsibility for, the content, privacy policies, or practices of any third party web sites or services. You further acknowledge and agree that georgiamotorcoach.com shall not be responsible or liable, directly or indirectly, for any damage or loss caused or alleged to be caused by or in connection with use of or reliance on any such content, goods or services available on or through any such web sites or services.</p>
+                            <p>We strongly advise you to read the terms and conditions and privacy policies of any third-party web sites or services that you visit.</p>
+                            <h4>Governing Law</h4>
+                            <p>These Terms shall be governed and construed in accordance with the laws of Georgia, United States, without regard to its conflict of law provisions.</p>
+                            <p>Our failure to enforce any right or provision of these Terms will not be considered a waiver of those rights. If any provision of these Terms is held to be invalid or unenforceable by a court, the remaining provisions of these Terms will remain in effect. These Terms constitute the entire agreement between us regarding our Service, and supersede and replace any prior agreements we might have between us regarding the Service.</p>
+                            <h4>Changes</h4>
+                            <p>We reserve the right, at our sole discretion, to modify or replace these Terms at any time. If a revision is material we will try to provide at least 30 days notice prior to any new terms taking effect. What constitutes a material change will be determined at our sole discretion.</p>
+                            <p>By continuing to access or use our Service after those revisions become effective, you agree to be bound by the revised terms. If you do not agree to the new terms, please stop using the Service.</p>
+                            <h4>Contact Us</h4>
+                            <p>If you have any questions about these Terms, please contact us.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+</body>
+<?php include ("footer.php");?>
+
+</html>
