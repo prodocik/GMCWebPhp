01d76f884d39ccea275a187ff98f69dd05a518c7
prodocik <prodocik@users.noreply.github.com>
2017-08-26 20:54:30 -0400

    vars
    variables changes

--- variables.php
+++ variables.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 //*******Settings********
 $geminiPrice = 189;
 $viewPrice = 189;
 $vegasPrice = 199;
 $cambriaPrice = 205;
 $nexusPrice = 199;
-$soleraPrice = 175; 
+$soleraPrice = 175;
 $btcruiserPrice = 155;
 
 $bigRVPrep = 105;
@@ -24,7 +24,6 @@ function format_num($in) {
 
 //*******Prices********
 ////Gemini/////
-<<<<<<< HEAD
 $geminiRegDaily =  format_num($geminiPrice);
 $geminiRegWeekly = format_num($geminiPrice*$weeklyMultiplier);
 $geminiRegMonthly = format_num($geminiPrice*$monthlyMultiplier);
@@ -108,108 +107,7 @@ $nexusLowWeekly = format_num($nexusPrice*$winterMultiplier*$weeklyMultiplier);
 $nexusLowMonthly = format_num($nexusPrice*$winterMultiplier*$monthlyMultiplier);
 $nexusPrepFee = format_num($bigRVPrep);
 
-=======
-$geminiRegDaily = '$199';
-$geminiRegWeekly = '$1,293.50';
-$geminiRegMonthly = '$4,139.20';
-$geminiHighDaily = '$208.95';
-$geminiHighWeekly = '$1,358.18';
-$geminiHighMonthly = '$4,346.16';
-$geminiLowDaily = '$179.10';
-$geminiLowWeekly = '$1,164.15';
-$geminiLowMonthly = '$3,725.28';
-$geminiPrepFee = '$95';
-////Vegas/////
-$vegasRegDaily = '$199';
-$vegasRegWeekly = '$1,293.50';
-$vegasRegMonthly = '$4,139.20';
-$vegasHighDaily = '$208.95';
-$vegasHighWeekly = '$1,358.18';
-$vegasHighMonthly = '$4,346.16';
-$vegasLowDaily = '$179.10';
-$vegasLowWeekly = '$1,164.15';
-$vegasLowMonthly = '$3,725.28';
-$vegasPrepFee = '$95';
-////Cambria/////
-$cambriaRegDaily = '$205';
-$cambriaRegWeekly = '$1,332.50';
-$cambriaRegMonthly = '$4,264';
-$cambriaHighDaily = '$215.25';
-$cambriaHighWeekly = '$1,399.13';
-$cambriaHighMonthly = '$4,477.20';
-$cambriaLowDaily = '$184.50';
-$cambriaLowWeekly = '$1,199.25';
-$cambriaLowMonthly = '$3,837.60';
-$cambriaPrepFee = '$105';
-////View/////
-$viewRegDaily = '$199';
-$viewRegWeekly = '$1,293.50';
-$viewRegMonthly = '$4,139.20';
-$viewHighDaily = '$208.95';
-$viewHighWeekly = '$1,358.18';
-$viewHighMonthly = '$4,346.16';
-$viewLowDaily = '$179.10';
-$viewLowWeekly = '$1,164.15';
-$viewLowMonthly = '$3,725.28';
-$viewPrepFee = '$95';
-////Lexington/////
-$lexRegDaily = '$175';
-$lexRegWeekly = '$1,137.50';
-$lexRegMonthly = '$3,640';
-$lexHighDaily = '$183.75';
-$lexHighWeekly = '$1,194.38';
-$lexHighMonthly = '$3,822';
-$lexLowDaily = '$157.50';
-$lexLowWeekly = '$1,023.75';
-$lexLowMonthly = '$3,276';
-$lexPrepFee = '$95';
-////Phoenix/////
-$phoenixRegDaily = '$155';
-$phoenixRegWeekly = '$1,007.50';
-$phoenixRegMonthly = '$3,224';
-$phoenixHighDaily = '$162.75';
-$phoenixHighWeekly = '$1,057.50';
-$phoenixHighMonthly = '$3,385.20';
-$phoenixLowDaily = '$139.50';
-$phoenixLowWeekly = '$906.75';
-$phoenixLowMonthly = '$2,901.60';
-$phoenixPrepFee = '$95';
-////btcruiser/////
-$btcruiserRegDaily = '$155';
-$btcruiserRegWeekly = '$1,007.50';
-$btcruiserRegMonthly =  '$3,224';
-$btcruiserHighDaily = '$162.75';
-$btcruiserHighWeekly = '$1,057.50';
-$btcruiserHighMonthly =  '$3,385.20';
-$btcruiserLowDaily = '$139.50';
-$btcruiserLowWeekly = '$906.75';
-$btcruiserLowMonthly = '$2,901.60';
-$btcruiserPrepFee = '$95';
-////Solera/////
-$soleraRegDaily = '$175';
-$soleraRegWeekly = '$1,137.50';
-$soleraRegMonthly = '$3,640';
-$soleraHighDaily = '$183.75';
-$soleraHighWeekly = '$1,194.38';
-$soleraHighMonthly = '$3,822';
-$soleraLowDaily = '$157.50';
-$soleraLowWeekly = '$1,023.75';
-$soleraLowMonthly = '$3,276';
-$soleraPrepFee = '$95';
-////Nexus/////
-$nexusRegDaily = '$199';
-$nexusRegWeekly = '$1,293.50';
-$nexusRegMonthly = '$4,139.20';
-$nexusHighDaily = '$208.95';
-$nexusHighWeekly = '$1,358.18';
-$nexusHighMonthly = '$4,346.16';
-$nexusLowDaily = '$179.10';
-$nexusLowWeekly = '$1,164.15';
-$nexusLowMonthly = '$3,725.28';
-$nexusPrepFee = '$105';
->>>>>>> origin/master
 //***************
 ///PhoneNumber
 $phoneNumber = "404-478-6454";
 ?>
-
